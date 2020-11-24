b80e73ffa5c4d91bee6465d3567ce25376b27604
SCG82 <scg082+github@gmail.com>
2020-11-23 23:57:24 -0800

    Perfect Negotiation Demo: move perfect negotiation logic to peer.js

--- src/content/peerconnection/perfect-negotiation/js/main.js
+++ src/content/peerconnection/perfect-negotiation/js/main.js
@@ -1,5 +1,5 @@
 /*
- *  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
+ *  Copyright (c) 2020 The WebRTC project authors. All Rights Reserved.
  *
  *  Use of this source code is governed by a BSD-style license
  *  that can be found in the LICENSE file in the root of the source
@@ -7,135 +7,8 @@
  */
 
 'use strict';
-// @ts-check
-/**
- * "Library code" for polite and impolite peers.
- */
-function peer(other, polite, fail = null) {
-  const send = (tgt, msg) => tgt.postMessage(JSON.parse(JSON.stringify(msg)), "*");
-  if (!fail) fail = e => send(window.parent, {error: `${e.name}: ${e.message}`});
-  const pc = new RTCPeerConnection();
 
-  if (!window.assert_equals) {
-    window.assert_equals = (a, b, msg) => a === b || fail(new Error(`${msg} expected ${b} but got ${a}`));
-  }
-
-  const log = str => { console.log((polite ? "[POLITE] " : "[IMPOLITE] ") + str); }
-
-  const localVideo1 = document.getElementById("localVideo1");
-  const localVideo2 = document.getElementById("localVideo2");
-  const remoteVideo = document.getElementById("remoteVideo");
-  const transceiversForSending = [];
-
-  const commands = {
-    async swapTransceivers() {
-      log("swapTransceivers");
-      const stream1 = localVideo1.srcObject;
-      const stream2 = localVideo2.srcObject;
-      if (transceiversForSending.length == 0) {
-        // This is the first time swapTransceivers is called.
-        // Add the initial transceivers, which are remembered for future swaps.
-        transceiversForSending.push(
-            pc.addTransceiver(stream1.getTracks()[0], {streams:[stream1], direction:"sendonly"}));
-        transceiversForSending.push(
-            pc.addTransceiver("video", {streams:[stream2], direction:"inactive"}));
-        return;
-      } else {
-        // We have sent before. Swap which transceiver is the sending one.
-        if (transceiversForSending[0].direction == "sendonly") {
-          transceiversForSending[0].direction = "inactive";
-          transceiversForSending[0].sender.replaceTrack(null);
-          transceiversForSending[1].direction = "sendonly";
-          transceiversForSending[1].sender.replaceTrack(stream2.getTracks()[0]);
-        } else {
-          transceiversForSending[1].direction = "inactive";
-          transceiversForSending[1].sender.replaceTrack(null);
-          transceiversForSending[0].direction = "sendonly";
-          transceiversForSending[0].sender.replaceTrack(stream1.getTracks()[0]);
-        }
-      }
-    },
-  };
-
-  try {
-    pc.ontrack = e => {
-      log("ontrack")
-      remoteVideo.srcObject = new MediaStream();
-      remoteVideo.srcObject.addTrack(e.track);
-    };
-  
-    pc.addEventListener("icecandidate", ({candidate}) => send(other, {candidate}));
-    let makingOffer = false, ignoreOffer = false;
-    let srdAnswerPending = false;
-    pc.addEventListener("negotiationneeded", async () => {
-      try {
-        log("SLD due to negotiationneeded")
-        assert_equals(pc.signalingState, "stable", "negotiationneeded always fires in stable state");
-        assert_equals(makingOffer, false, "negotiationneeded not already in progress");
-        makingOffer = true;
-        await pc.setLocalDescription();
-        assert_equals(pc.signalingState, "have-local-offer", "negotiationneeded not racing with onmessage");
-        assert_equals(pc.localDescription.type, "offer", "negotiationneeded SLD worked");
-        send(other, {description: pc.localDescription});
-      } catch (e) {
-        fail(e);
-      } finally {
-        makingOffer = false;
-      }
-    });
-    window.onmessage = async ({data: {description, candidate, run}}) => {
-      try {
-        if (description) {
-          // If we have a setRemoteDescription() answer operation pending, then
-          // we will be "stable" by the time the next setRemoteDescription() is
-          // executed, so we count this being stable when deciding whether to
-          // ignore the offer.
-          let isStable =
-              pc.signalingState == "stable" ||
-              (pc.signalingState == "have-local-offer" && srdAnswerPending);
-          ignoreOffer =
-              description.type == "offer" && !polite && (makingOffer || !isStable);
-          if (ignoreOffer) {
-            log("glare - ignoring offer");
-            return;
-          }
-          if (description.type == "answer") {
-            srdAnswerPending = true;
-          }
-          log("SRD(" + description.type +")");
-          await pc.setRemoteDescription(description);
-          srdAnswerPending = false;
-          if (description.type == "offer") {
-            assert_equals(pc.signalingState, "have-remote-offer", "Remote offer");
-            assert_equals(pc.remoteDescription.type, "offer", "SRD worked");
-            log("SLD to get back to stable")
-            await pc.setLocalDescription();
-            assert_equals(pc.signalingState, "stable", "onmessage not racing with negotiationneeded");
-            assert_equals(pc.localDescription.type, "answer", "onmessage SLD worked");
-            send(other, {description: pc.localDescription});
-          } else {
-            assert_equals(pc.remoteDescription.type, "answer", "Answer was set");
-            assert_equals(pc.signalingState, "stable", "answered");
-            pc.dispatchEvent(new Event("negotiated"));
-          }
-        } else if (candidate) {
-          try {
-            await pc.addIceCandidate(candidate);
-          } catch (e) {
-            if (!ignoreOffer) throw e;
-          }
-        } else if (run) {
-          send(window.parent, {[run.id]: await commands[run.cmd]() || 0});
-        }
-      } catch (e) {
-        fail(e);
-      }
-    };
-  } catch (e) {
-    fail(e);
-  }
-  return pc;
-}
+import peer from './peer.js';
 
 let counter = 0;
 async function run(target, cmd) {
@@ -147,6 +20,7 @@ async function run(target, cmd) {
     r(data[id]);
   }));
 }
+window.run = run;
 
 function startLocalVideo(r1,g1,b1, r2,g2,b2) {
   const whiteNoise = (width, height, r, g, b) => {
@@ -175,29 +49,16 @@ function startLocalVideo(r1,g1,b1, r2,g2,b2) {
   localVideo2.play();
 }
 
-/**
- * Setup iframes.
- */
-
 const politeIframe = document.getElementById("polite");
 const impoliteIframe = document.getElementById("impolite");
 
-async function setupIframes() {
-  await setupIframe(politeIframe, 'polite', 0,1,0, 0,1,1);
-  await setupIframe(impoliteIframe, 'impolite', 1,0,0, 1,0,1);
-  // await setupPoliteIframe();
-  // await setupImpoliteIframe();
-}
-setupIframes();
-
 async function setupIframe(el, polite, r1,g1,b1, r2,g2,b2) {
-  const role = polite ? 'Polite' : 'Impolite';
   el.srcdoc =
     `<html\>
   <body\>
-  <p\>${role} Peer's iframe</p\>
+  <p\>${polite ? 'Polite' : 'Impolite'} Peer's iframe</p\>
   <p\>
-    <button onclick="(${startLocalVideo.toString()})(${r1,g1,b1,r2,g2,b2});"\>Start</button\>
+    <button onclick="(${startLocalVideo.toString()})(${r1},${g1},${b1},${r2},${g2},${b2});"\>Start</button\>
     <button onclick="window.parent.run(window, 'swapTransceivers')"\>Swap Sending Track</button\>
   </p\>
   <p\>
@@ -207,57 +68,17 @@ async function setupIframe(el, polite, r1,g1,b1, r2,g2,b2) {
   </p\>
   </body\>
   <script\>
-    (${peer.toString()})(window.parent.document.getElementById("${polite ? 'impolite' : 'polite'}").contentWindow, false);
+    (${peer.toString()})(window.parent.document.getElementById("${polite ? 'impolite' : 'polite'}").contentWindow, ${polite});
   </script\>
   <html\>`;
   await new Promise(r => el.onload = r);
 }
 
-/*
-async function setupPoliteIframe() {
-  politeIframe.srcdoc =
-    `<html\>
-  <body\>
-  <p\>Polite Peer's iframe</p\>
-  <p\>
-    <button onclick="(${startLocalVideo.toString()})(0,1,0,0,1,1);"\>Start</button\>
-    <button onclick="window.parent.run(window, 'swapTransceivers')"\>Swap Sending Track</button\>
-  </p\>
-  <p\>
-    <video id="localVideo1" autoplay\></video\>
-    <video id="localVideo2" autoplay\></video\>
-    <video id="remoteVideo" autoplay\></video\>
-  </p\>
-  </body\>
-  <script\>
-    (${peer.toString()})(window.parent.document.getElementById("impolite").contentWindow, true);
-  </script\>
-  <html\>`;
-  await new Promise(r => politeIframe.onload = r);
+async function setupIframes() {
+  await setupIframe(politeIframe, 'polite', 0,1,0, 0,1,1);
+  await setupIframe(impoliteIframe, 'impolite', 1,0,0, 1,0,1);
 }
-
-async function setupImpoliteIframe() {
-  impoliteIframe.srcdoc =
-    `<html\>
-  <body\>
-  <p\>Impolite Peer's iframe</p\>
-  <p\>
-    <button onclick="(${startLocalVideo.toString()})(1,0,0,1,0,1);"\>Start</button\>
-    <button onclick="window.parent.run(window, 'swapTransceivers')"\>Swap Sending Track</button\>
-  </p\>
-  <p\>
-    <video id="localVideo1" autoplay\></video\>
-    <video id="localVideo2" autoplay\></video\>
-    <video id="remoteVideo" autoplay\></video\>
-  </p\>
-  </body\>
-  <script\>
-    (${peer.toString()})(window.parent.document.getElementById("polite").contentWindow, false);
-  </script\>
-  <html\>`;
-  await new Promise(r => impoliteIframe.onload = r);
-}
-*/
+setupIframes();
 
 async function swapOnBoth(politeFirst) {
   if (politeFirst) {
